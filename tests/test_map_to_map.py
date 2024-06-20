4e1573cc49ad25c946690a4ad24051e9ba17476a
Geoffrey Woollard <geoffwoollard@gmail.com>
2024-06-20 16:39:28 -0400

    unit test

--- tests/test_map_to_map.py
+++ tests/test_map_to_map.py
@@ -1,5 +1,7 @@
-def test_pass():
-    assert True
+from omegaconf import OmegaConf
+from cryo_challenge._commands import run_map2map_pipeline
 
-def test_fail():
-    assert False
+args = OmegaConf.create({'config': 'config_files/test_config_map_to_map_distance_matrix.yaml'})
+
+def test_run_map2map_pipeline(args):    
+    run_map2map_pipeline.main(args)
