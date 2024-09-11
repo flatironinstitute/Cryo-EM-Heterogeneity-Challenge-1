6def1bf6e6a20a4c3f50652002f23eb5f53fc1c2
Geoffrey Woollard <geoffwoollard@gmail.com>
2024-09-10 21:15:21 -0400

    tests for masking and normalization

--- tests/test_map_to_map.py
+++ tests/test_map_to_map.py
@@ -4,31 +4,37 @@ import numpy as np
 
 
 def test_run_map2map_pipeline():
-    args = OmegaConf.create(
-        {"config": "tests/config_files/test_config_map_to_map.yaml"}
-    )
-    results_dict = run_map2map_pipeline.main(args)
+    for config_fname, config_fname_low_memory in zip(
+        [
+            "tests/config_files/test_config_map_to_map.yaml",
+            "tests/config_files/test_config_map_to_map.yaml",
+        ],
+        [
+            "tests/config_files/test_config_map_to_map_low_memory.yaml",
+            "tests/config_files/test_config_map_to_map_low_memory.yaml",
+        ],
+    ):
+        args = OmegaConf.create({"config": config_fname})
+        results_dict = run_map2map_pipeline.main(args)
 
-    args_low_memory = OmegaConf.create(
-        {"config": "tests/config_files/test_config_map_to_map_low_memory.yaml"}
-    )
-    results_dict_low_memory = run_map2map_pipeline.main(args_low_memory)
-    for metric in ["fsc", "corr", "l2", "bioem"]:
-        if metric == "fsc":
+        args_low_memory = OmegaConf.create({"config": config_fname_low_memory})
+        results_dict_low_memory = run_map2map_pipeline.main(args_low_memory)
+        for metric in ["fsc", "corr", "l2", "bioem"]:
+            if metric == "fsc":
+                np.allclose(
+                    results_dict[metric]["computed_assets"]["fsc_matrix"],
+                    results_dict_low_memory[metric + "_low_memory"]["computed_assets"][
+                        "fsc_matrix"
+                    ],
+                )
+            elif metric == "res":
+                np.allclose(
+                    results_dict[metric]["computed_assets"]["fraction_nyquist"],
+                    results_dict_low_memory[metric + "_low_memory"]["computed_assets"][
+                        "fraction_nyquist"
+                    ],
+                )
             np.allclose(
-                results_dict[metric]["computed_assets"]["fsc_matrix"],
-                results_dict_low_memory[metric + "_low_memory"]["computed_assets"][
-                    "fsc_matrix"
-                ],
+                results_dict[metric]["cost_matrix"].values,
+                results_dict_low_memory[metric + "_low_memory"]["cost_matrix"].values,
             )
-        elif metric == "res":
-            np.allclose(
-                results_dict[metric]["computed_assets"]["fraction_nyquist"],
-                results_dict_low_memory[metric + "_low_memory"]["computed_assets"][
-                    "fraction_nyquist"
-                ],
-            )
-        np.allclose(
-            results_dict[metric]["cost_matrix"].values,
-            results_dict_low_memory[metric + "_low_memory"]["cost_matrix"].values,
-        )
