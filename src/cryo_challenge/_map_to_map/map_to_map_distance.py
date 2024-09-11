22ec5a5085cb8a8eaac1d54549ff6296dfa1bde1
Geoffrey Woollard <geoffwoollard@gmail.com>
2024-09-10 20:50:09 -0400

    all metrics implemented and test passing for matched results

--- src/cryo_challenge/_map_to_map/map_to_map_distance.py
+++ src/cryo_challenge/_map_to_map/map_to_map_distance.py
@@ -543,6 +543,7 @@ class FSCResDistance(MapToMapDistance):
 
     def __init__(self, config):
         super().__init__(config)
+        self.fsc_label = "fsc"
 
     @override
     def get_distance_matrix(
@@ -553,7 +554,7 @@ class FSCResDistance(MapToMapDistance):
             self.config["data"]["n_pix"] // 2
         )  # TODO: check for odd psizes if this should be +1
         psize = self.config["data"]["psize"]
-        fsc_matrix = global_store_of_running_results["fsc"]["computed_assets"][
+        fsc_matrix = global_store_of_running_results[self.fsc_label]["computed_assets"][
             "fsc_matrix"
         ]
         units_Angstroms = (
@@ -570,7 +571,7 @@ class FSCResDistance(MapToMapDistance):
         return units_Angstroms[res_fsc_half]
 
 
-class FSCResDistanceLowMemory(MapToMapDistance):
+class FSCResDistanceLowMemory(FSCResDistance):
     """FSC Resolution distance.
 
     The resolution at which the Fourier Shell Correlation reaches 0.5.
@@ -579,28 +580,4 @@ class FSCResDistanceLowMemory(MapToMapDistance):
 
     def __init__(self, config):
         super().__init__(config)
-
-    @override
-    def get_distance_matrix(
-        self, maps1, maps2, global_store_of_running_results
-    ):  # custom method
-        # get fsc matrix
-        fourier_pixel_max = (
-            self.config["data"]["n_pix"] // 2
-        )  # TODO: check for odd psizes if this should be +1
-        psize = self.config["data"]["psize"]
-        fsc_matrix = global_store_of_running_results["fsc_low_memory"][
-            "computed_assets"
-        ]["fsc_matrix"]
-        units_Angstroms = (
-            2 * psize / (np.arange(1, fourier_pixel_max + 1) / fourier_pixel_max)
-        )
-
-        def res_at_fsc_threshold(fscs, threshold=0.5):
-            res_fsc_half = np.argmin(fscs > threshold, axis=-1)
-            fraction_nyquist = 0.5 * res_fsc_half / fscs.shape[-1]
-            return res_fsc_half, fraction_nyquist
-
-        res_fsc_half, fraction_nyquist = res_at_fsc_threshold(fsc_matrix)
-        self.stored_computed_assets = {"fraction_nyquist": fraction_nyquist}
-        return units_Angstroms[res_fsc_half]
+        self.fsc_label = "fsc_low_memory"
