a703ba040216f169311272f8e48f87fe698c3b2b
Geoffrey Woollard <geoffwoollard@gmail.com>
2025-01-15 19:20:19 -0500

    no slurm runner import

--- src/cryo_challenge/_map_to_map/map_to_map_distance.py
+++ src/cryo_challenge/_map_to_map/map_to_map_distance.py
@@ -7,7 +7,7 @@ from typing_extensions import override
 import mrcfile
 import numpy as np
 from dask.distributed import Client
-from dask_hpc_runner import SlurmRunner
+# from dask_hpc_runner import SlurmRunner
 
 from .gromov_wasserstein.gw_weighted_voxels import get_distance_matrix_dask_gw
 
@@ -499,25 +499,26 @@ class GromovWassersteinDistance(MapToMapDistance):
         maps2 = maps2.reshape((len(maps2),) + maps1.shape[1:])
 
         if extra_params["slurm"]:
-            job_id = os.environ["SLURM_JOB_ID"]
-            scheduler_file = os.path.join(
-                extra_params["scheduler_file_dir"], f"scheduler-{job_id}.json"
-            )
-            with SlurmRunner(
-                scheduler_file=scheduler_file,
-            ) as runner:
-                # The runner object contains the scheduler address and can be passed directly to a client
-                with Client(runner) as client:
-                    distance_matrix_dask_gw = get_distance_matrix_dask_gw(
-                        volumes_i=maps1,
-                        volumes_j=maps2,
-                        top_k=extra_params["top_k"],
-                        n_downsample_pix=extra_params["n_downsample_pix"],
-                        exponent=extra_params["exponent"],
-                        cost_scale_factor=extra_params["cost_scale_factor"],
-                        scheduler=extra_params["scheduler"],
-                        element_wise=extra_params["element_wise"],
-                    )
+            pass
+            # job_id = os.environ["SLURM_JOB_ID"]
+            # scheduler_file = os.path.join(
+            #     extra_params["scheduler_file_dir"], f"scheduler-{job_id}.json"
+            # )
+            # with SlurmRunner(
+            #     scheduler_file=scheduler_file,
+            # ) as runner:
+            #     # The runner object contains the scheduler address and can be passed directly to a client
+            #     with Client(runner) as client:
+            #         distance_matrix_dask_gw = get_distance_matrix_dask_gw(
+            #             volumes_i=maps1,
+            #             volumes_j=maps2,
+            #             top_k=extra_params["top_k"],
+            #             n_downsample_pix=extra_params["n_downsample_pix"],
+            #             exponent=extra_params["exponent"],
+            #             cost_scale_factor=extra_params["cost_scale_factor"],
+            #             scheduler=extra_params["scheduler"],
+            #             element_wise=extra_params["element_wise"],
+            #         )
 
         else:
             local_directory = extra_params["local_directory"]
