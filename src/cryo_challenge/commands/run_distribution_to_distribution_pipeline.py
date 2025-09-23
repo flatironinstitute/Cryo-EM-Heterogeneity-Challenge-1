c649e2b1c0f95e384d7eb18a5a59db5560358ddf
Geoffrey Woollard <geoffwoollard@gmail.com>
2025-09-23 19:18:41 -0400

    d2d notebook. config, paths

--- src/cryo_challenge/commands/run_distribution_to_distribution_pipeline.py
+++ src/cryo_challenge/commands/run_distribution_to_distribution_pipeline.py
@@ -7,9 +7,7 @@ import os
 import yaml
 
 from ..distribution_to_distribution.distribution_to_distribution import run
-from ..config_validation._distribution_to_distribution_validation import (
-    DistToDistInputConfig,
-)
+from ..config_validation import DistToDistInputConfig
 
 
 def add_args(parser):
