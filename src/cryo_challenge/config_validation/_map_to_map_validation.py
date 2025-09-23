11c0314e5b99b84335ca9d4ba48af11fb3c0ae97
Geoffrey Woollard <geoffwoollard@gmail.com>
2025-09-23 17:39:35 -0400

    update notebook. update config used in notebook. update validator to handle optional outputs that are None

--- src/cryo_challenge/config_validation/_map_to_map_validation.py
+++ src/cryo_challenge/config_validation/_map_to_map_validation.py
@@ -112,7 +112,6 @@ class MapToMapInputConfigData(BaseModel, extra="forbid"):
         description="Parameters for the ground truth file",
     )
     mask_params: dict = Field(
-        default=dict(apply_mask=False, path_to_mask=None),
         description="Parameters for the mask file",
     )
 
@@ -584,7 +583,7 @@ class MapToMapResultsAllMetrics(BaseModel, extra="forbid"):
         default=None,
         description="Cost matrix (ground truth versus submission)",
     )
-    cost_matrix_self: pandas.DataFrame = Field(
+    cost_matrix_self: Optional[pandas.DataFrame] = Field(
         default=None,
         description="Cost matrix (submission versus submission)",
     )
@@ -596,7 +595,7 @@ class MapToMapResultsAllMetrics(BaseModel, extra="forbid"):
         default=None,
         description="Computed assets",
     )
-    computed_assets_self: dict = Field(
+    computed_assets_self: Optional[dict] = Field(
         default=None,
         description="Computed assets",
     )
